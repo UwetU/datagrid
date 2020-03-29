09eea3c6162c8a0c1b4d609dcb91f48a6f327ee0
UwetU <grafcf@gmail.com>
2020-03-29 17:16:27 +0500

    feat: add sort on all col and add indication sort

--- src/actions.js
+++ src/actions.js
@@ -3,4 +3,7 @@ export const search = (e) => ({
     valueSearch: e.target.value
 });
 
-export const sort = () => ({ type: 'SORT' });
+export const sort = (e, sortBy) => ({
+    type: sortBy ? 'SORT_DESC' : 'SORT_ASC',
+    el: e.target.textContent ? e.target.textContent : e.target.value
+});
